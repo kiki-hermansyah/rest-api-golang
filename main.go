5d0d027c0646518de14cca5651d461b518ed47f8
test <test@gmail.com>
2022-06-07 13:51:20 +0700

    first commit

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"rest-go-demo/database"
+	"rest-go-demo/entity"
+	"rest-go-demo/route"
+
+	"github.com/gorilla/mux"
+	_ "github.com/jinzhu/gorm/dialects/mysql" //Required for MySQL dialect
+)
+
+func main() {
+	initDB()
+	log.Println("Starting the HTTP server on port 8090")
+	router := mux.NewRouter().StrictSlash(true)
+	route.Route(router)
+	log.Fatal(http.ListenAndServe(":8090", router))
+}
+
+func initDB() {
+	config :=
+		database.Config{
+			ServerName: "localhost:3306",
+			User:       "root",
+			Password:   "",
+			DB:         "go",
+		}
+
+	connectionString := database.GetConnectionString(config)
+	err := database.Connect(connectionString)
+	if err != nil {
+		panic(err.Error())
+	}
+	database.Migrate(&entity.Person{})
+}
---
+++ route/api.go
@@ -0,0 +1,15 @@
+package route
+
+import (
+	"rest-go-demo/controllers"
+
+	"github.com/gorilla/mux"
+)
+
+func Route(router *mux.Router) {
+	router.HandleFunc("/create", controllers.CreatePerson).Methods("POST")
+	router.HandleFunc("/get", controllers.GetAllPerson).Methods("GET")
+	router.HandleFunc("/get/{id}", controllers.GetPersonByID).Methods("GET")
+	router.HandleFunc("/update/{id}", controllers.UpdatePersonByID).Methods("PUT")
+	router.HandleFunc("/delete/{id}", controllers.DeletPersonByID).Methods("DELETE")
+}
